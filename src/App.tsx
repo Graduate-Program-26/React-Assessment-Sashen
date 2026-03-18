22778611936e4bb85bbe54177842c96dcafe19b4
Sashen <SGovindasamy@dvtsoftware.com>
2026-03-18 11:02:27 +0200

    Initial commit - installing shadcn setup + tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <div className="flex min-h-screen items-center justify-center">
+      <Button>shadcn is working</Button>
+    </div>
   )
 }
 
