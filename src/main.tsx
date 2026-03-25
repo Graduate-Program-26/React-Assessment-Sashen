9ae2d391a5e4ac4ee28da94cd65c73de23e44398
Sashen <SGovindasamy@dvtsoftware.com>
2026-03-25 13:16:17 +0200

    chore: fix ThemeProvider prop alignment and missing semicolon in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProvider from "@/components/ThemeProvider";
 import './index.css';
 import App from './App.tsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
